6df52c03a23fd59d606e411fbd2d20b8a502ae71
Ye Htun (hENRY) <87560504+yehtunaung@users.noreply.github.com>
2025-09-30 23:53:32 +0630

    update fix pe academic

--- app/Models/PEAcademicQualifications.php
+++ app/Models/PEAcademicQualifications.php
@@ -5,19 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class PEAcademicQualifications extends Model
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class PEAcademicQualifications extends Model implements HasMedia
 {
+    use SoftDeletes, InteractsWithMedia;
     use SoftDeletes;
     protected $table = "pe_academic_qualifications";
 
     protected $fillable = [
         'pe_form_id',
-        'university_id',
-        'graduation_year',
-        'graduation_month',
-        'engineering_discipline_id',
-        'academic_qualification_id',
-        'qualification_name',
-        'qualification_type',
+        'first_university_id',
+        'first_graduation_year',
+        'first_eng_disc_id',
+        'first_acad_qual_id',
+        'post_university_id',
+        'post_graduation_year',
+        'post_eng_disc_id',
+        'post_acad_qual_id',
+        'other_eng_disc_id',
+        'other_graduation_year',
+        'other_document_name_1',
+        'other_document_name_2',
+        'other_document_name_3',
+        'other_document_name_4',
     ];
 }
--- database/migrations/2025_09_08_140756_create_pe_academic_qualifications_table.php
+++ database/migrations/2025_09_08_140756_create_pe_academic_qualifications_table.php
@@ -13,14 +13,42 @@ return new class extends Migration
     {
         Schema::create('pe_academic_qualifications', function (Blueprint $table) {
             $table->id();
+
             $table->foreignId('pe_form_id')->constrained('pe_registration_forms')->onDelete('cascade');
-            $table->foreignId('university_id')->nullable()->constrained('universities');
-            $table->integer('graduation_year')->nullable();
-            $table->integer('graduation_month')->nullable();
-            $table->foreignId('engineering_discipline_id')->nullable()->constrained('engineering_disciplines');
-            $table->foreignId('academic_qualification_id')->nullable()->constrained('academic_qualifications');
-            $table->string('qualification_name')->nullable();
-            $table->enum('qualification_type', ['first', 'post', 'other']);
+            $table->foreignId('first_university_id')->nullable()->constrained('universities');
+
+            $table->year('first_graduation_year')->nullable();
+
+            $table->foreignId('first_eng_disc_id')->nullable()->constrained('engineering_disciplines');
+            $table->foreignId('first_acad_qual_id')
+                ->nullable()
+                ->constrained('academic_qualifications');
+
+            $table->foreignId('post_university_id')
+                ->nullable()
+                ->constrained('universities');
+
+            $table->year('post_graduation_year')->nullable();
+
+            $table->foreignId('post_eng_disc_id')
+                ->nullable()
+                ->constrained('engineering_disciplines');
+
+            $table->foreignId('post_acad_qual_id')
+                ->nullable()
+                ->constrained('academic_qualifications');
+
+            $table->foreignId('other_eng_disc_id')
+                ->nullable()
+                ->constrained('engineering_disciplines');
+
+            $table->year('other_graduation_year')->nullable();
+
+            $table->string('other_document_name_1')->nullable();
+            $table->string('other_document_name_2')->nullable();
+            $table->string('other_document_name_3')->nullable();
+            $table->string('other_document_name_4')->nullable();
+
             $table->timestamps();
             $table->softDeletes();
         });
